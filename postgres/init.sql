2ebe7176b773231668bdd52742d758d8971d3ac5
alingigraphael123 <raphael.alingig@traxtech.com>
2026-08-18 03:34:44 +0800

    Added pg in docker

---
+++ postgres/init.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA IF NOT EXISTS data_norm AUTHORIZATION admin;
+GRANT ALL ON SCHEMA data_norm TO admin;
+
+CREATE SCHEMA IF NOT EXISTS predata_validation AUTHORIZATION admin;
+GRANT ALL ON SCHEMA predata_validation TO admin;
